8fe3a567c3796164fa259a05338240f728ce898e
Hasegawa-UD <kaiton2721@gmail.com>
2026-05-16 15:13:28 +0900

    kadai2

---
+++ kadai2/kadai2.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+
+
+int main()
+{
+    int ages[4];
+    ages[0] = 15;
+    ages[1] = 32;
+    ages[2] = 11;
+    ages[3] = 48;
+
+    for (int i = 0; i <= 3; i++) {
+        int val = ages[i];
+
+        printf("年齢は %d です\n", val);
+    }
+}
