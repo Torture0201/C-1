c69d6ab5b380f4a1531baf76ea1d4b26c67ec6ba
XPS\ktc <>
2026-06-06 13:18:19 +0900

    a

---
+++ obaro/kadai/kadai.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int addint(int a, int b, int c);
+float addfloat(float a, float b);
+double adddouble(double q, double p);
+
+int main()
+{
+    addint(1, 2, 3);
+    addfloat(10, 20);
+    adddouble(100, 200);
+
+}
+
+int addint(int a, int b, int c)
+{
+    int x = a + b + c;
+    printf("int関数の足し算　結果%d\n", x);
+    return 0;
+}
+
+float addfloat(float a, float b)
+{
+    float y = a + b;
+    printf("float関数の足し算　結果%f\n", y);
+    return 0;
+}
+
+double adddouble(double a, double b)
+{
+    double z = a + b;
+    printf("double関数の足し算　結果%f\n", z);
+    return 0;
+}
